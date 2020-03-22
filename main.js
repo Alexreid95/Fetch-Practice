6325fc5598b5ae1a5f423c06cbc15d3d162b4871
Alexreid95 <alexreid95@gmail.com>
2020-03-22 22:14:17 +0000

    Display multi results from fetch

--- main.js
+++ main.js
@@ -1,49 +1,55 @@
 // -----------Global variables -----------
 // Search bar
 const searchBarBtn = document.querySelector(".search-bar__btn");
-// Fetch car results
+// Fetch
 let cardContainer = document.querySelector(".card-container")
 
 // -----------Functions -----------
 // Resets display and content 
 function reset() {
-	cardContainer.style.display = "none";
+	cardContainer.style.visibility = "hidden";
 	cardContainer.value = "";
 }
 
 // Fetch from Reddit API
 function fetchSub(sub) {
 	fetch(
-			`https://api.pushshift.io/reddit/search/submission/?q=${sub}&is_video=true&subreddit=css&subreddit=WebdevTutorials&subreddit=web_design&subreddit=webdev`
+			`https://api.pushshift.io/reddit/search/submission/?q=${sub}&size=8&is_video=true&subreddit=css&subreddit=WebdevTutorials&subreddit=web_design&subreddit=webdev`
 		)
 		.then(res => res.json())
 		.then(res => {
-			let selectResult = res.data[0];
-			displayInfo({
-				title: selectResult.title,
-				sub: selectResult.subreddit,
-				url: selectResult.url,
-				link: selectResult.full_link
-			});
+			for (let i = 0; i < res.data.length; i++) {
+				// Selects each array element from fetch result
+				let selectResult = res.data[i];
+				// Creates an empty element to store fetch result
+				createCard();
+				// Gave each card a numbered class
+				cardContainer.children[i].classList.add(`card-${i}`);
+				// Grabs elements
+				let title = document.querySelector(`.card-${i} > .info__title`);
+				let sub = document.querySelector(`.card-${i} > .info__sub`);
+				let url = document.querySelector(`.card-${i} > .info__url`);
+				let link = document.querySelector(`.card-${i} > .info__link`);
+
+				// Grabs result and store it into the DOM
+				let result = ({
+					title: selectResult.title,
+					sub: selectResult.subreddit,
+					url: selectResult.url,
+					link: selectResult.full_link
+				});
+
+				title.textContent = result.title;
+				sub.textContent = result.sub;
+				url.textContent = result.url;
+				link.textContent = result.link;
+			}
+
 		})
 		.catch(console.error);
 }
 
-// Displays information into the DOM
-function displayInfo(object) {
-	createCard();
-	const title = document.querySelector(".info__title");
-	const sub = document.querySelector(".info__sub");
-	const url = document.querySelector(".info__url");
-	const link = document.querySelector(".info__link");
-
-	title.textContent = object.title;
-	sub.textContent = object.sub;
-	url.textContent = object.url;
-	link.textContent = object.link;
-}
-
-// Creates card form template
+// Creates card from template
 function createCard() {
 	let temp = document.querySelector(".card-template");
 	let clone = temp.content.cloneNode(true);
@@ -53,7 +59,7 @@ function createCard() {
 // Search bar functionality 
 function searchTheInput() {
 	reset();
-	cardContainer.style.display = "block";
+	cardContainer.style.visibility = "visible";
 	let searchInput = document.querySelector("#search-bar__input");
 	(searchInput.value === "") ? alert("Please input text"): fetchSub(searchInput.value);
 }
